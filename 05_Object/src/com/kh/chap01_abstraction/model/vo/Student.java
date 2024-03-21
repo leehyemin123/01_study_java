319c271c6242f74af6666d409e528ec0d16874a5
leehyemin123 <151510167+leehyemin123@users.noreply.github.com>
2024-03-21 23:54:53 +0900

    자바(05_Object_8일차)

---
+++ 05_Object/src/com/kh/chap01_abstraction/model/vo/Student.java
@@ -0,0 +1,34 @@
+package com.kh.chap01_abstraction.model.vo;
+
+public class Student {
+
+	/*
+	 * * 클래스의 구조 
+	 * public class 클래스명 { 
+	 * 
+	 *
+	 *  // 필드부 
+	 *
+	 *  // 생성자부 
+	 *  
+	 *  // 메소드부 
+	 *  public void mehotd1(){
+	 *  
+	 *  }
+	 *  
+	 * } 
+	 *
+	 */
+	
+	
+	//  필드부 
+	//  접근제한자 [예약어] 자료형 변수명
+	
+	// * 접근제한자 : 여기에 접근할 수 있는 범위를 제한하는 것
+	// 				public > protected > default > private
+	
+		public String name; // 이름
+		public int age; // 나이 
+		public double heigth; // 키
+	}
+
---
+++ 05_Object/src/com/kh/chap01_abstraction/run/AbstractionRun.java
@@ -0,0 +1,10 @@
+package com.kh.chap01_abstraction.run;
+
+public class AbstractionRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
